dff683b1efe8299217990c2cf24ffbbad4d0a95e
Yonathan Mekonnen <mekonneny@Yonathans-MacBook-Air.local>
2017-10-12 21:16:47 -0400

    Posting the App

---
+++ SchedulingApp/app/src/test/java/schedulingapp/team17/cmsc355/com/schedulingapp/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package schedulingapp.team17.cmsc355.com.schedulingapp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
