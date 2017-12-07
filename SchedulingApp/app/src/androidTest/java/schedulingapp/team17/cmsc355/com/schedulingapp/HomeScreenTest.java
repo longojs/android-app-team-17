98e562d9861c59621a1a18e9817e65bc6b8616de
Yonathan Mekonnen <mekonneny@Yonathans-MacBook-Air.local>
2017-12-07 12:45:29 -0500

    Update Layout

--- SchedulingApp/app/src/androidTest/java/schedulingapp/team17/cmsc355/com/schedulingapp/HomeScreenTest.java
+++ SchedulingApp/app/src/androidTest/java/schedulingapp/team17/cmsc355/com/schedulingapp/HomeScreenTest.java
@@ -11,11 +11,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
-<<<<<<< HEAD
- * Created by mekonneny on 11/17/17.
-=======
  * Created by josephlongo on 11/17/17.
->>>>>>> e41dc4eb6229401f6e07c6d8ea9c8a3bf36e3f79
  */
 public class HomeScreenTest {
     @Rule
@@ -23,25 +19,10 @@ public class HomeScreenTest {
 
     private HomeScreen mActivity = null;
 
-<<<<<<< HEAD
     @Before
     public void setUp() throws Exception {
+        mActivity = colorsActivityTestRule.getActivity();
 
-       mActivity = colorsActivityTestRule.getActivity();
-
-    }
-
-    @Test
-    public void testLaunch()
-    {
-        View view = mActivity.findViewById(R.id.button28);
-
-        assertNotNull(view);
-=======
-
-    @Before
-    public void setUp() throws Exception {
-     mActivity = colorsActivityTestRule.getActivity();
     }
 
     @Test
@@ -57,16 +38,11 @@ public class HomeScreenTest {
 
         View viewPurple = mActivity.findViewById(R.id.button27);
         assertNotNull(viewPurple);
->>>>>>> e41dc4eb6229401f6e07c6d8ea9c8a3bf36e3f79
     }
 
     @After
     public void tearDown() throws Exception {
         mActivity = null;
     }
-
-<<<<<<< HEAD
-
-=======
->>>>>>> e41dc4eb6229401f6e07c6d8ea9c8a3bf36e3f79
 }
+
