61884f0bbd8b1cbff6ee2b987c7948078ff33358
Joseph Longo <josephlongo@Josephs-MacBook-Pro.local>
2017-12-07 13:48:53 -0500

    I hate everything

--- SchedulingApp/app/src/main/java/schedulingapp/team17/cmsc355/com/schedulingapp/ProfileCreator.java
+++ SchedulingApp/app/src/main/java/schedulingapp/team17/cmsc355/com/schedulingapp/ProfileCreator.java
@@ -29,7 +29,7 @@ public class ProfileCreator extends AppCompatActivity {
         setContentView(R.layout.activity_profile_creator);
         nameInput = (EditText) findViewById(R.id.nameInput);
         emailInput = (EditText) findViewById(R.id.emailInput);
-       // passwordInput = (EditText) findViewById(R.id.passwordInput);
+        //passwordInput = (EditText) findViewById(R.id.passwordInput);
         view = this.getWindow().getDecorView();
         submitButton = (Button) findViewById(R.id.submitButton);
         submitButton.setOnClickListener(new View.OnClickListener() {
